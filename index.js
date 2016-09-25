f626a0c9548b2a7e9ee3741873dcf87a1424ca21
Said Sef <saidsef@economist.com>
2016-09-25 12:39:52 +0100

    fixed multi container docker container config, nginx and host tunning

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
+'use strict';
 var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 80;
-var randomNumber = (Math.random() * 100);
+var randomNumber = parseFloat((Math.random() * 100)).toFixed(2);
 
 app.get("/", function(req, res, next) {
   res.set('Content-Type', 'text/plain');
