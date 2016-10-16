b2b36b72288c987def8498cd859a137cbdb1b890
Said Sef <saidsef@gmail.com>
2016-10-16 02:01:59 +0100

    build script - supress docker image delete error

--- build.sh
+++ build.sh
@@ -22,7 +22,8 @@ cleanup() {
 delete() {
   echo "Remove dorment images"
   if [ ! -z "$(docker images -a -q | head -n 1)" ]; then
-    docker rmi -f $(docker images -a -q)
+    docker rmi -f $(docker images -a -q) 2> /dev/null
+
   else
     echo "There is no dorment images"
   fi
