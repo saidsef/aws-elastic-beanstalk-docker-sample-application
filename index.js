9d9425d68945cec48de7e45dcabd08f615b36f16
Said Sef <saidsef@gmail.com>
2016-10-22 01:47:26 +0100

    print application json response

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 80;
 var randomNumber = parseFloat((Math.random() * 100)).toFixed(2);
 
 app.get("*", function(req, res, next) {
-  var buildID = fs.readFile("build_id.txt", "utf-8");
+  var buildID = fs.readFileSync("build_id.txt", "utf-8").trim();
   res.json({ "message": "Hello World!", "random number": parseInt(String(randomNumber),10), "version": buildID });
 });
 
