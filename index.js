fd393f33380a1b4e7dc7add5ebfe881b071fc36b
Said Sef <saidsef@gmail.com>
2016-10-22 14:21:28 +0100

    response json chnaged from varsion to build

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var randomNumber = parseFloat((Math.random() * 100)).toFixed(2);
 
 app.get("*", function(req, res, next) {
   var buildID = fs.readFileSync("build_id.txt", "utf-8").trim();
-  res.json({ "message": "Hello World!", "random number": parseInt(String(randomNumber),10), "version": buildID });
+  res.json({ "message": "Hello World!", "random number": parseInt(String(randomNumber),10), "build": buildID });
 });
 
 app.listen(port);
