a92df967ed5de6d0f21cc840c9eb6a635cb79eb6
saidsef <saidsef@gmail.com>
2018-11-09 16:42:51 +0000

    updated welcome message

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 80;
 app.get("*", function(req, res, next) {
   let randomNumber = parseInt(String(parseFloat((Math.random() * 100)).toFixed(2)),10);
   let buildID = fs.readFileSync("build_id.txt", "utf-8").trim();
-  res.json({ "message": "Hello World!", "random number": randomNumber, "build": buildID });
+  res.json({ "message": "Hello World!, Welcome to my world.", "random number": randomNumber, "build": buildID });
 });
 
 app.listen(PORT);
